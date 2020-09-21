a120d421f7cc4077ddd3fa9250d1c2b1879df9fb
Shahreen Shahjahan Psyche <shahreeen@gmail.com>
2020-09-21 10:59:41 -0500

    Files updateed

--- SourceCode/process_data_for_annotation.py
+++ SourceCode/process_data_for_annotation.py
@@ -3,44 +3,46 @@ import path
 import SourceCode.utils as u
 
 
+def convert_to_set(col):
+
+    event_set = set()
+    for i in col:
+        event_set.add(i)
+    return event_set
+
+def probable_relevant_story(news, event_set, threshold):
+    tokenized = news.split(" ")
+    count = 0
+    for t in tokenized:
+        if t in event_set:
+            count += 1
+        if count >= threshold:
+            return True
+    return False
+
+
 if __name__ == "__main__":
 
     source_data_path = path.MASTER
     event_path = path.TAGTOG_POST_ANNOTATION
-    destination_data_path = path.TAGTOG_POST_ANNOTATION
+    destination_data_path = path.TAGTOG_PRE_ANNOTATION
 
     action_file = path.ACTION
     source_file = path.FROM_2011To2015
 
     path_name = source_data_path + source_file
     action_path = event_path + action_file
-    
+
 
     master_data = pd.read_csv(path_name, header=[0])
     master_data = u.remove_special_characters(master_data, 'news')
 
     actions = pd.read_csv(action_path, header=[0])
+    actions_set = convert_to_set(actions.iloc[:, 0])
 
 
     # Step - 1 : Pull Out All the Probable relevant story
+    master_data['Relevance'] = master_data['news'].apply(probable_relevant_story, args = (actions_set, 4))
+    print(master_data[master_data['Relevance'] == True].shape)
 
-
-
-    # Step - 2 : Get a proportionate data for each country and each year - 750 * 3 - Total data
-
-
-
-    # Step - 3 : Divide the data into 3 parts
-
-
-    print(master_data.head(10))
-
-    # df = pd.read_csv("news_sample_data_16000.tsv", sep='\t')
-    # prefix = 'news_satp_'
-    # chunk_size = int(df.shape[0] / 32)
-    # i = 4
-    # for start in range(0, df.shape[0], chunk_size):
-    #     df_subset = df.iloc[start:start + chunk_size]
-    #     df_subset.to_csv('splitted_data/' + prefix + str(i) + '.tsv', index=False, header=True, sep='\t')
-    #     i += 1
-    #     print(df_subset.shape)
+    master_data.to_csv(destination_data_path+'master_2011to2015_probable_relevant_marked_data.csv', header=True, index=True, index_label='index')
--- SourceCode/sample_data_for_annotation.py
+++ SourceCode/sample_data_for_annotation.py
@@ -3,13 +3,11 @@ import path
 import sklearn
 
 if __name__ == "__main__":
+
     source_root = path.TAGTOG_PRE_ANNOTATION
-    source_file = "master_2011to2015_probable_relevant_data.csv"
+    source_file = "master_2011to2015_probable_relevant_marked_data.csv"
     source_path = source_root + source_file
     destination = path.TAGTOG_PRE_ANNOTATION + "TagTog_Data/"
-    total_data = 9000
-    file_index = [i for i in range(10, 22)]
-    file_prefix = 'news_satp_'
 
     master_data = pd.read_csv(source_path, header=[0])
 
@@ -18,20 +16,20 @@ if __name__ == "__main__":
 
     print(master_data['location'].value_counts())
 
-    n = master_data.shape[0]
-    sample_df = master_data.groupby('Relevance').apply(lambda x: x.sample(21500))
+    # n = relevant.shape[0]
+    # sample_df = master_data.groupby('Relevance').apply(lambda x: x.sample(21000))
+
+    # print(sample_df['Relevance'].value_counts())
+    # print(sample_df['location'].value_counts())
+
+    n = relevant.shape[0]
+    sample_df = relevant.groupby('year').apply(lambda x: x.sample(3000))
 
     print(sample_df['Relevance'].value_counts())
     print(sample_df['location'].value_counts())
 
     n = sample_df.shape[0]
-    sample_df = sample_df.groupby('year').apply(lambda x: x.sample(4000))
-
-    print(sample_df['Relevance'].value_counts())
-    print(sample_df['location'].value_counts())
-
-    n = sample_df.shape[0]
-    sample_df = sample_df.groupby('location').apply(lambda x: x.sample(frac = 0.5))
+    sample_df = sample_df.groupby('location').apply(lambda x: x.sample(frac = 0.6))
 
     print(sample_df['Relevance'].value_counts())
     print(sample_df['location'].value_counts())
@@ -42,6 +40,15 @@ if __name__ == "__main__":
     sample_df.reset_index(drop=True, inplace=True)
     print(sample_df.head(5))
     print(sample_df['Relevance'].value_counts())
+
+    print(sample_df['Relevance'])
+
+    sample_df.to_csv(destination + "sampled_9000_relevant_data.csv", header=True, index = False)
+
+
+
+
+
 
 
 
